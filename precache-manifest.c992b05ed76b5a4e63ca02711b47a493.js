c518635ae584d0478a67fddfcf8b61541adadc33
unknown <you@example.com>
2020-10-01 19:05:48 +0530

    Updates

---
+++ precache-manifest.c992b05ed76b5a4e63ca02711b47a493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2e3f6c9cb11e1a0b7ca7f06977f60b",
+    "url": "/Dinda-Academy/index.html"
+  },
+  {
+    "revision": "f3a582669a16ddd88aa9",
+    "url": "/Dinda-Academy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "88714a8dd8fdcfa4362a",
+    "url": "/Dinda-Academy/static/css/main.5d4ddcce.chunk.css"
+  },
+  {
+    "revision": "f3a582669a16ddd88aa9",
+    "url": "/Dinda-Academy/static/js/2.6f9412be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dinda-Academy/static/js/2.6f9412be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88714a8dd8fdcfa4362a",
+    "url": "/Dinda-Academy/static/js/main.4c16b084.chunk.js"
+  },
+  {
+    "revision": "8f5531870cd8a369fd1d",
+    "url": "/Dinda-Academy/static/js/runtime-main.74803c90.js"
+  }
+]);
